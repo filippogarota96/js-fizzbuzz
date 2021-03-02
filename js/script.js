9f9aa73e7098d35ae4c9519378154a5388187b2c
Filippo Garota <filogarota101@gmail.com>
2021-03-02 15:16:42 +0100

    Stampo numeri da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+// Stampo numeri da 1 a cento
+
+var numeri;
+
+for ( var numeri = 1; numeri <= 100; numeri++) {
+  console.log(numeri);
+}
